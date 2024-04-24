ed872f4aa389d600197da9e5a1a1dc22af457cbe
yang_zhi_hang <727842516@qq.com>
2024-04-24 04:27:38 -0400

    framework: Encapsulate the Home page as a separate file.

--- cgi-bin/myapp.py
+++ cgi-bin/myapp.py
@@ -1,37 +1,9 @@
 # app.py
 from flask_restful import Resource, Api, reqparse, abort
 import json
+import home
 
-def get_original_string():
-    original_string = """
-    {
-        "MsgID": 10,
-        "Result": 0,
-        "data": {
-            "cpu_usage": "1.23%",
-            "cpu_temperature": "47.00℃",
-            "total_memory": "1992288KB",
-            "memory_usage": "288800KB",
-            "status_4g": "离线",
-            "network_type": "LET(4G)",
-            "online_time": "0s",
-            "signal_strength": "0%",
-            "wifi_status": "无驱动",
-            "network_name": "IIOT_Gateway",
-            "ip_address": "192.168.2.1",
-            "wifi_strength": "75%",
-            "device_status": "正常",
-            "data_period": "300ms",
-            "device_code": "Positive coater",
-            "protocol_type": "S7_1500",
-            "server_status": "在线",
-            "report_period": "231ms",
-            "server_ip": "172.17.91.130",
-            "north_protocol_type": "opc-ua"
-        }
-    }
-    """
-    return json.loads(original_string)
+
 
 class commonhandle(Resource):
 
@@ -48,13 +20,11 @@ class commonhandle(Resource):
 
         if MsgID == 16:
             if private_data['user'] == 'iiot' and private_data['password'] == '123456':
-                # print(private_data['user'], private_data['password'])
                 return {'MsgID':MsgID, 'Result': 0, 'data':{'pass':1}}
             else:
-                # print(private_data['user'], private_data['password'])
                 return {'MsgID':MsgID, 'Result': 0, 'data':{'pass':-2}}
         elif MsgID == 10:
-            return get_original_string()
+            return home.get_original_string()
         else:
             return {'MsgID':MsgID, 'Result': 1}
 
